50817304866fca699d716d8c75bcafd2e5cff8db
kom2703-cloud <kom2703@gmail.com>
2026-06-04 14:11:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,3 +16,5 @@ elif st.button("💰💰การวิเคราะห์ข้อมูล�
     st.switch_page("pages/EDA_app.py")
 elif st.button("💰💰การพยากรณ์ยอดจ่ายแบบง่าย"):
     st.switch_page("pages/sale_predict.py")
+elif st.button("💰💰การพยากรณ์ระยะเวลาการให้บริการขนส่ง"):
+    st.switch_page("pages/truck_predict.py")
