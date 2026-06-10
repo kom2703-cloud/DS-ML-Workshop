8bb50c164e903620fe6c55c82453f2899629ad47
kom2703-cloud <kom2703@gmail.com>
2026-06-10 15:14:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,7 @@ elif st.button("💰💰การพยากรณ์ยอดจ่ายแ�
     st.switch_page("pages/sale_predict.py")
 elif st.button("💰💰การพยากรณ์ระยะเวลาการให้บริการขนส่ง"):
     st.switch_page("pages/truck_predict.py")
-elif st.button("💰💰การจัดกล่มแบบ Clustering"):
+elif st.button("💰💰การจัดกล่มข้อมูล"):
     st.switch_page("pages/clustering_segment.py")    
+elif st.button("💰💰ระบบแนะนำสินค้าจาก Market Basket Analysis"):
+    st.switch_page("pages/association_items.py")  
